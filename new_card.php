b2e7f3463343b90a845e6a97bd546d6ab8fa9a29
Kaiyu Zheng <kaiyutony@gmail.com>
2014-11-05 11:52:39 -0800

    Added new_card page

--- new_card.php
+++ new_card.php
@@ -16,8 +16,59 @@ require_once "template.php";
   <body>
     <?php 
     top_bar();
+    card_form();
+    preview_card();
     ?>
   </body>
 </html>
 <?php 
+function card_form()
+{
 ?>
+  <div class="card-form">
+    <form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post">
+      <?php 
+      card_front();
+      card_back();
+      ?>
+      <input type="submit" value="Done" id="submit" name="card_submit" class="submit-card"/>
+    </form>
+  </div>
+<?php
+}
+
+function card_front()
+{
+?>
+  <div class="card-frame" id="card_front_edit">
+   Title: <input class="card-field" id="card_name" type="text" name="card_name"  />
+    Subdescription: <input class="card-sub" id="card_sub" type="text" name="card_sub" />
+  </div>
+<?php
+}
+
+function card_back()
+{
+?>
+  <div class="card-frame" id="card_back_edit">
+    <textarea class="card-textarea" id="card_content" name="card_content" resizable="false" value="Put a rich text editor here."></textarea>
+  </div>
+<?php
+}
+
+function preview_card()
+{
+?>
+  <div class="preview-card">
+    <h2>Preview</h2>
+    <div class="card-frame" id="card_front_preview">
+      <h4>Front</h4>
+    </div>
+    <div class="card-frame" id="card_back_preview">
+      <h4>Back</h4>
+    </div>
+  </div>
+<?php
+}
+?>
+
