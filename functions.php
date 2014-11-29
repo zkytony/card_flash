88eaa25a929cf0280c5c85e6252cff3099684e68
Kaiyu Zheng <kaiyutony@gmail.com>
2014-11-28 21:33:56 -0800

    Filtering dangerous html tags

---
+++ functions.php
@@ -0,0 +1,34 @@
+<?php
+/* 
+   functions that mostly serve as utility
+   current functions:
+   filter_html_tags();
+*/
+
+// Reads in a string containing html entities, filter out
+// potentially harmful tags such as script;
+function filter_html_tags($html_str)
+{
+  $harm_tags = array("script", "embed", "link", 
+                     "listing", "meta", "noscript", 
+                     "object", "plaintext", "xmp");
+
+  for ($i=0; $i<sizeof($harm_tags); $i++)
+  {
+    // the regex for matching the tags and their children
+    // '-' appearing in the front and the end is the delimeter
+    $match = '-<' . $harm_tags[$i] . '[^>]*>';
+    if ($harm_tags[$i] != "meta" && $harm_tags[$i] != "link")
+    {
+      $match .= '.*?</' . $harm_tags[$i] . '>?-i';
+    } else {
+      $match .= "-i"; // adding ending delimeter
+    }
+    $html_str = preg_replace($match, "WARNING", $html_str);
+  }
+  
+  return $html_str;
+}
+
+?>
+
---
+++ script/functions.js
@@ -0,0 +1,19 @@
+// Functions of utility use
+
+function filterHTMLTags(htmlStr) {
+    var harmTags = ["script", "embed", "link",
+                    "listing", "meta", "noscript", 
+                    "object", "plaintext", "xmp"];
+    
+    for (var i = 0; i < harmTags.length; i++) {
+        var matchStr = "<" + harmTags[i] + "[^>]*>";
+
+        if (harmTags[i] != "meta" && harmTags[i] != "link")
+        { 
+            matchStr += ".*?</" + harmTags[i] + ">?";
+        }
+        var regObj = new RegExp(matchStr, "i");
+        htmlStr = htmlStr.replace(regObj, "WARNING");
+    }
+    return htmlStr;
+}
