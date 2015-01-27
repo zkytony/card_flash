7058dbe2c1d262dbe658cfbd63202dd4d65f75bc
Kaiyu Zheng <kaiyutony@gmail.com>
2015-01-26 18:17:22 -0800

    Improved Circle class

--- model/circle.php
+++ model/circle.php
@@ -16,7 +16,7 @@ class Circle
   private $exist;
 
   function __construct($circleid, $con) {
-    $result = select_from("circles", "*", "WHERE `circleid` = '$circleid'", $con);
+    $result = select_from("circles", "*", " WHERE `circleid` = '$circleid'", $con);
     $this->exist = $result->num_rows == 1;
     if ($this->exist) {
       $this->circleid = $circleid;
@@ -39,72 +39,72 @@ class Circle
   public static function create($userid, $title, $forwhat, $con) {
     // insert into circles
 
-    $result = select_from("circles", "*", "", $con);
+    $result = select_from("circles", "`circleid`", "", $con);
     $circleid = "crc_" . $result->num_rows;
     $circleid = ensure_unique_id($circleid, "circles", "circleid", $con);
 
     $columns = "`circleid`, `userid`, `title`, `forwhat`, `create_time`, `member_count`, `admin_count`";
-    $values = "'$circleid', '$userid', '$title', '$forwhat', NOW(), '1', '1'");
+    $values = "'$circleid', '$userid', '$title', '$forwhat', NOW(), '0', '0'";
     insert_into("circles", $columns, $values, $con);
 
     // insert into members
-
-    $result = select_from("members", "*", "", $con);
-    $memberid = "crc_" . $result->num_rows;
-    $memberid = ensure_unique_id($memberid, "members", "memberid", $con);
-
-    $columns = "`memberid`, `circleid`, `userid`, `role`, `join_time`, `last_activity_time`";
-    $values = "'$memberid', '$circleid', '$userid', '0', NOW(), NOW()";
-    insert_into("members", $columns, $values, $con);
-    
-    Circle::member_count_add_one($circleid, true, $con);
+    Circle::add_member($circleid, $userid, 0, $con);
+    return $circleid;
   }
 
   public static function member_count_add_one($circleid, $admin, $con) {
     $restrict_str="WHERE `circleid`='$circleid'";
-    $columns = array("`member_count`");
-    $values = array("`member_count`+1");
+    $columns = array();
+    $values = array();
     if ($admin) {
       $columns[] = "`admin_count`";
-      $values[] = "`admin_count`+1");
+      $values[] = "`admin_count`+1";
+    } else {
+      $columns[] = "`member_count`";
+      $values[] = "`member_count`+1";
     }
-    update_table("users", $columns, $values, $restrict_str, $con);
+    update_table("circles", $columns, $values, $restrict_str, $con);
   }
 
   public static function member_count_subtract_one($circleid, $admin, $con) {
     $restrict_str="WHERE `circleid`='$circleid'";
-    $columns = array("`member_count`");
-    $values = array("`member_count`-1");
+    $columns = array();
+    $values = array();
     if ($admin) {
       $columns[] = "`admin_count`";
-      $values[] = "`admin_count`-1");
+      $values[] = "`admin_count`-1";
+    } else {
+      $columns[] = "`member_count`";
+      $values[] = "`member_count`-1";
     }
-    update_table("users", $columns, $values, $restrict_str, $con);
+    update_table("circles", $columns, $values, $restrict_str, $con);
   }
 
+  // 0 - admin --> the ability to kick people out, and assign other people as admin
+  // 1 - normal --> normal abilities: leave, invite others, see updates
   public static function add_member($circleid, $userid, $role, $con) {
     $result = select_from("members", "*", "", $con);
-    $memberid = "crc_" . $result->num_rows;
+    $memberid = "mem_" . $result->num_rows;
     $memberid = ensure_unique_id($memberid, "members", "memberid", $con);
 
     $columns = "`memberid`, `circleid`, `userid`, `role`, `join_time`, `last_activity_time`";
     $values = "'$memberid', '$circleid', '$userid', '$role', NOW(), NOW()";
     insert_into("members", $columns, $values, $con);
     
-    $admin = false;
-    if ($role == '0') $admin = true;
+    $admin = $role == 0;
     Circle::member_count_add_one($circleid, $admin, $con);
+    return $memberid;
   }
 
   public static function remove_member($circleid, $userid, $con) {
-    delete_from("members", 
-                "WHERE `circleid` = '$circleid' AND `userid` = '$userid'", $con);
     $admin = Circle::is_admin_of($circleid, $userid, $con);
     Circle::member_count_subtract_one($circleid, $admin, $con);
+    delete_from("members", 
+                "WHERE `circleid` = '$circleid' AND `userid` = '$userid'", '', $con);
   }
 
   public static function delete_circle($circleid, $con) {
-    delete_from("circles", "WHERE `circleid` = '$circleid'", $con);
+    delete_from("circles", "WHERE `circleid` = '$circleid'", '', $con);
   }
 
   public static function is_admin_of($circleid, $userid, $con) {
@@ -115,5 +115,54 @@ class Circle
     return false;
   }
 
+  public static function get_memberid($circleid, $userid, $con) {
+    $result = select_from("members", "`memberid`", "WHERE `circleid` = '$circleid' AND `userid` = '$userid'", $con);
+    while ($row = mysqli_fetch_assoc($result)) {
+      return $row['memberid'];
+    }
+    return NULL;
+  }
+
+  // returns an array of userids of a particular circle, with a particular role
+  // if $role is set to a negative number, then select all members
+  // 0 - admin --> the ability to kick people out, and assign other people as admin
+  // 1 - normal --> normal abilities: leave, invite others, see updates
+  public static function get_members($circleid, $role, $con) {
+    $restrict_str = "WHERE `circleid` = '$circleid' ";
+    if ($role >= 0) {
+      $restrict_str .= "AND `role` = '$role'";
+    }
+
+    $result = select_from("members", "`userid`", $restrict_str, $con);
+    $id_arr = array();
+    while ($row = mysqli_fetch_assoc($result)) {
+      $id_arr[] = $row['userid'];
+    }
+    return $id_arr;
+  }
+
+  // Returns the number of members in a circle given its circleid
+  // Specify the role of the member wanted:
+  // 0 - admin
+  // 1 - normal
+  // Returns -1 if the role is invalid
+  public static function num_members($circleid, $role, $con) {
+    $result = "";
+    if ($role == 0) {
+      $result = select_from("circles", "`admin_count`", "WHERE `circleid` = '$circleid'", $con);
+    } else if ($role == 1) {
+      $result = select_from("circles", "`member_count`", "WHERE `circleid` = '$circleid'", $con);
+    } else {
+      return -1;
+    }
+    while ($row = mysqli_fetch_assoc($result)) {
+      if ($role == 0) {
+        return $row['admin_count'];
+      } else {
+        return $row['member_count'];
+      }
+    }
+    return 0;
+  }
 }
 ?>
