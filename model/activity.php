fe71c7d23aeafc9f06ddffbd8ca20220b959a5bd
Kaiyu Zheng <kaiyutony@gmail.com>
2015-02-18 21:12:24 -0800

    Used STR_TO_DATE() to convert datetime string to MySQL format datetime

--- model/activity.php
+++ model/activity.php
@@ -53,6 +53,7 @@ class Activity
   // table, $data will be an array of the information necessary
   // The keys are specific Strings that will be defined below:
   // 'time' : the time string that activity happens in datetime format
+  //        Format: %H:%i:%S,%m-%d-%Y. Example: 02:53:23,4-5-1995
   // 'userid' : the userid of the user that is the subject of the activity
   // 'deckid' : the deckid of the associated deck, if there is one
   // 'cardid' : the deckid of the associated card, if there is one
@@ -76,14 +77,21 @@ class Activity
   // 'userfollow' : specific data for user follow activity
   //      - 'targetid' : the userid who is followed
   //      - 'following' : (BOOL) true if following; false if unfollow
+  //
+  // Returns the timeid for this activity in timeline table, if successfully added;
+  // otherwise, returns NULL
   public static add_activity($type, $data, $con) {
+
+    $id = '';
+    $tablename = '';
+
     switch ($type) {
 
       case 0: // user register
         $tablename = "activity_user_register";
         $id = make_id("reg", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `time`";
-        $values = "'$id', '{$data['userid']}', '{$data['time']}";
+        $values = "'$id', '{$data['userid']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -92,7 +100,7 @@ class Activity
         $id = make_id("dkn", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `deckid`, `first`, `circleid`, `new`, `time`";
         $values = "'$id', '{$data['userid']}', '{$data['deckid']}', "
-                ."'{$data['newdeck']['first']}', '{$data['circleid']}', '{$data['newdeck']['new']}', '{$data['time']}";
+                ."'{$data['newdeck']['first']}', '{$data['circleid']}', '{$data['newdeck']['new']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -101,7 +109,7 @@ class Activity
         $id = make_id("cdn", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `deckid`, `cardid`, `circleid`, `new`, `time`";
         $values = "'$id', '{$data['userid']}', '{$data['deckid']}', "
-                ."'{$data['cardid']}', '{$data['circleid']}', '{$data['newcard']['new']}', '{$data['time']}";
+                ."'{$data['cardid']}', '{$data['circleid']}', '{$data['newcard']['new']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -110,7 +118,7 @@ class Activity
         $id = make_id("tch", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `deckid`, `added_tags`, `circleid`, `time`";
         $values = "'$id', '{$data['userid']}', '{$data['deckid']}', "
-                ."'{$data['tagchange']['added_tags']}', '{$data['circleid']}', '{$data['time']}";
+                ."'{$data['tagchange']['added_tags']}', '{$data['circleid']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -119,7 +127,7 @@ class Activity
         $id = make_id("dks", $tablename, "actid", $con);
         $columns = "`actid`, `from_userid`, `to_userid`, `deckid`, `circleid`, `sharing`, `time`";
         $values = "'$id', '{$data['deckshare']['from_userid']}', '{$data['deckshare']['to_userid']}', '{$data['deckid']}', "
-                ."'{$data['circleid']}', '{$data['deckshare']['sharing']}', '{$data['time']}";
+                ."'{$data['circleid']}', '{$data['deckshare']['sharing']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -127,7 +135,7 @@ class Activity
         $tablename = "activity_deck_subscribe";
         $id = make_id("sub", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `deckid`, `circleid`, `subscribing`, `time`";
-        $values = "'$id', '{$data['userid']}', '{$data['deckid']}', '{$data['circleid']}', '{$data['subscribe']['subscribing']}', '{$data['time']}'";
+        $values = "'$id', '{$data['userid']}', '{$data['deckid']}', '{$data['circleid']}', '{$data['subscribe']['subscribing']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -135,7 +143,7 @@ class Activity
         $tablename = "activity_group_join";
         $id = make_id("jgp", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `circleid`, `init`, `time`";
-        $values = "'$id', '{$data['userid']}', '{$data['deckid']}', '{$data['circleid']}', '{$data['joingroup']['init']}', '{$data['time']}'";
+        $values = "'$id', '{$data['userid']}', '{$data['deckid']}', '{$data['circleid']}', '{$data['joingroup']['init']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -143,7 +151,7 @@ class Activity
         $tablename = "activity_user_follow";
         $id = make_id("fol", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `targetid`, `following`, `time`";
-        $values = "'$id', '{$data['userid']}', '{$data['userfollow']['targetid']}', '{$data['userfollow']['following']}', '{$data['time']}'";
+        $values = "'$id', '{$data['userid']}', '{$data['userfollow']['targetid']}', '{$data['userfollow']['following']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
@@ -151,21 +159,30 @@ class Activity
         $tablename = "activity_deck_edited";
         $id = make_id("dup", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `deckid`, `circleid`, `time`";
-        $values = "'$id', '{$data['userid']}', '{$data['deckid']}', '{$data['circleid']}', '{$data['time']}";
+        $values = "'$id', '{$data['userid']}', '{$data['deckid']}', '{$data['circleid']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
       case 9: // a card's information is edited
         $tablename = "activity_card_edited";
-        $id = make_id("dup", $tablename, "actid", $con);
+        $id = make_id("cup", $tablename, "actid", $con);
         $columns = "`actid`, `userid`, `cardid`, `circleid`, `time`";
-        $values = "'$id', '{$data['userid']}', '{$data['cardid']}', '{$data['circleid']}', '{$data['time']}";
+        $values = "'$id', '{$data['userid']}', '{$data['cardid']}', '{$data['circleid']}', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
         insert_into($tablename, $columns, $values, $con);
         break;
 
       default:
         return NULL;
     }
+
+    // now $id is the id for that particular activity table
+    // And $tablename is the name of the particular table
+    $timeid = make_id("time", "timeline", "timeid", $con);
+    $columns = "`timeid`, `userid`, `refid`, `reftable`, `type`, `time`";
+    $values = "'$timeid', '{$data['userid']}', '$id', '$tablename', STR_TO_DATE(\"{$data['time']}\", \"%H:%i:%S,%m-%d-%Y\")";
+    insert_into('timeline', $columns, $values, $con);
+
+    return $timeid;
   }
 }
 
