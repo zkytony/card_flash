c9e6f3a0c0c16d88e14656c8b1983ac33a0cfbcd
Kaiyu Zheng <kaiyutony@gmail.com>
2015-03-28 15:51:45 -0700

    Use favorite.php to replace subscribe.php in models.php

--- models.php
+++ models.php
@@ -11,7 +11,7 @@ require_once "model/deck.php";
 require_once "model/tag.php";
 require_once "model/share.php";
 require_once "model/follower.php";
-require_once "model/subscriber.php";
+require_once "model/favorite.php";
 require_once "model/circle.php";
 require_once "model/activity.php";
 require_once "model/comment.php";
